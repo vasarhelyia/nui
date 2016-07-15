f39bc25a605afdd587dae62008ab1339faa5788a
vasarhelyia <vasarhelyi.agnes@ustream.tv>
2016-07-15 15:59:51 +0200

    Add xcode project with framework target

---
+++ NUI/NUI.h
@@ -0,0 +1,19 @@
+//
+//  NUI.h
+//  NUI
+//
+//  Created by Agnes Vasarhelyi on 15/07/16.
+//  Copyright © 2016 nui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NUI.
+FOUNDATION_EXPORT double NUIVersionNumber;
+
+//! Project version string for NUI.
+FOUNDATION_EXPORT const unsigned char NUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NUI/PublicHeader.h>
+
+
